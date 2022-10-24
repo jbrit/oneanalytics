625b53c9683baa327352e2bcd95ea53665fb9168
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-24 22:08:29 +0100

    feat : build home content component

---
+++ layout/HomeContent/HomeContent.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HomeContent = styled.section`
+  width: 80%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+`;
