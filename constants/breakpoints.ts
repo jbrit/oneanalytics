d7a643de562eff2e08e806035be0e28b887e3c79
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-24 08:36:33 +0100

    chore : add breakpoints

---
+++ constants/breakpoints.ts
@@ -0,0 +1,10 @@
+import { Breakpoints } from "$types/global";
+
+export const BREAKPOINTS: Breakpoints = {
+  sm: "min-width: 576px",
+  md: "min-width: 768px",
+  lg: "min-width: 992px",
+  xl: "min-width: 1200px",
+  xxl: "min-width: 1600px",
+  xxxl: "min-width: 2560px",
+};
