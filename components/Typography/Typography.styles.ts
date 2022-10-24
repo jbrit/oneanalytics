a0360335887d3d5f4ed41062d68dda51f039f508
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-24 08:36:18 +0100

    feat : build Typography component

---
+++ components/Typography/Typography.styles.ts
@@ -0,0 +1,75 @@
+import { BREAKPOINTS } from "$constants/breakpoints";
+import styled from "styled-components";
+import { ITypographyProps } from "./Typography.types";
+
+export const TypographyOne = styled.p<ITypographyProps>`
+  font-weight: ${({ weight }) => weight ?? "400"};
+  font-size: 1.2rem;
+  line-height: 2rem;
+  color: ${({ color }) => color ?? "rgba(255, 255, 255, 0.6)"};
+  margin-bottom: ${({ mb }) => mb};
+  word-break: break-all;
+  text-align: ${({ textAlign }) => textAlign};
+  @media screen and (${BREAKPOINTS.md}) {
+    font-size: 1.4rem;
+    line-height: 2rem;
+  }
+  @media screen and (${BREAKPOINTS.lg}) {
+    font-size: 1.6rem;
+    line-height: 2.6rem;
+  }
+`;
+
+export const TypographyTwo = styled.p<ITypographyProps>`
+  font-weight: ${({ weight }) => weight ?? "500"};
+  font-size: 1.4rem;
+  line-height: 2rem;
+  color: ${({ color }) => color ?? "rgba(255, 255, 255, 0.67)"};
+  margin-bottom: ${({ mb }) => mb};
+  word-break: break-all;
+  text-align: ${({ textAlign }) => textAlign};
+  @media screen and (${BREAKPOINTS.md}) {
+    font-size: 1.6rem;
+    line-height: 2.6rem;
+  }
+  @media screen and (${BREAKPOINTS.lg}) {
+    font-size: 1.8rem;
+    line-height: 3.1rem;
+  }
+`;
+
+export const TypographyThree = styled.p<ITypographyProps>`
+  font-weight: ${({ weight }) => weight ?? "500"};
+  font-size: 1.6rem;
+  line-height: 2.6rem;
+  color: ${({ color }) => color ?? "rgba(255, 255, 255, 0.67)"};
+  margin-bottom: ${({ mb }) => mb};
+  word-break: break-all;
+  text-align: ${({ textAlign }) => textAlign};
+  @media screen and (${BREAKPOINTS.md}) {
+    font-size: 1.8rem;
+    line-height: 3.1rem;
+  }
+  @media screen and (${BREAKPOINTS.lg}) {
+    font-size: 2rem;
+    line-height: 3.4rem;
+  }
+`;
+
+export const TypographyFour = styled.p<ITypographyProps>`
+  font-weight: ${({ weight }) => weight ?? "500"};
+  font-size: 2.4rem;
+  line-height: 3.4rem;
+  color: ${({ color }) => color ?? "rgba(255, 255, 255, 0.67)"};
+  margin-bottom: ${({ mb }) => mb};
+  word-break: break-all;
+  text-align: ${({ textAlign }) => textAlign};
+  @media screen and (${BREAKPOINTS.md}) {
+    font-size: 2.6rem;
+    line-height: 4rem;
+  }
+  @media screen and (${BREAKPOINTS.lg}) {
+    font-size: 2.8rem;
+    line-height: 4.2rem;
+  }
+`;
---
+++ components/Typography/Typography.types.ts
@@ -0,0 +1,8 @@
+import CSSTypes from "csstype";
+
+export interface ITypographyProps {
+  mb?: CSSTypes.Property.MarginBottom;
+  textAlign?: CSSTypes.Property.TextAlign;
+  color?: CSSTypes.Property.Color;
+  weight?: CSSTypes.Property.FontWeight;
+}
