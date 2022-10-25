2e19422989452264560587e97f90b56293ac47f5
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-25 09:04:48 +0100

    chore : setup react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,8 @@ import { chains, wagmiClient } from "$utils/rainbow";
 import { GlobalStyles } from "styles/Global";
 import { FontStyles } from "styles/Fonts";
 import "antd/dist/antd.css";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider);
@@ -15,16 +17,19 @@ function getLibrary(provider: any): Web3Provider {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider chains={chains}>
-          <FontStyles />
-          <GlobalStyles />
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </Web3ReactProvider>
+    <QueryClientProvider client={queryClient}>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider chains={chains}>
+            <FontStyles />
+            <GlobalStyles />
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </Web3ReactProvider>
+    </QueryClientProvider>
   );
 }
 
