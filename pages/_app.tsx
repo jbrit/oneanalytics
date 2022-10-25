a629be7b87dfa2e8f064ac34b7927a598057bce4
jibola <pro.ajibolaojo@gmail.com>
2022-10-25 09:47:04 +0100

    commented out dapp parts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,34 @@
 import type { AppProps } from "next/app";
-import { Web3ReactProvider } from "@web3-react/core";
-import { Web3Provider } from "@ethersproject/providers";
-import { WagmiConfig } from "wagmi";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chains, wagmiClient } from "$utils/rainbow";
+// import { Web3ReactProvider } from "@web3-react/core";
+// import { Web3Provider } from "@ethersproject/providers";
+// import { WagmiConfig } from "wagmi";
+// import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+// import { chains, wagmiClient } from "$utils/rainbow";
 import { GlobalStyles } from "styles/Global";
 import { FontStyles } from "styles/Fonts";
 import "antd/dist/antd.css";
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-function getLibrary(provider: any): Web3Provider {
-  const library = new Web3Provider(provider);
-  library.pollingInterval = 12000;
-  return library;
-}
+// function getLibrary(provider: any): Web3Provider {
+//   const library = new Web3Provider(provider);
+//   library.pollingInterval = 12000;
+//   return library;
+// }
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
-      <Web3ReactProvider getLibrary={getLibrary}>
-        <WagmiConfig client={wagmiClient}>
-          <RainbowKitProvider chains={chains}>
+      {/* <Web3ReactProvider getLibrary={getLibrary}> */}
+        {/* <WagmiConfig client={wagmiClient}> */}
+          {/* <RainbowKitProvider chains={chains}> */}
             <FontStyles />
             <GlobalStyles />
             <Component {...pageProps} />
-          </RainbowKitProvider>
-        </WagmiConfig>
-      </Web3ReactProvider>
+          {/* </RainbowKitProvider> */}
+        {/* </WagmiConfig> */}
+      {/* </Web3ReactProvider> */}
     </QueryClientProvider>
   );
 }
